c615befedc009541946d14dc1c9605b7cb653b77
Trang Lu <lu.t.t.trang@gmail.com>
2026-03-05 13:38:33 +0700

    ex lesson 2 JavaScript

---
+++ tests/lesson-02/ex2.js
@@ -0,0 +1,3 @@
+const myName = "Alex";
+//myName = "Nagi"; biến hằng không gán lại
+console.log(myName);
---
+++ tests/lesson-02/ex3.js
@@ -0,0 +1,15 @@
+
+/* 
+câu hỏi
+biến: a = true, b = false, c =true.
+a. a && b && c
+b. a && b || c
+Đáp án:
+a. false
+b.true
+*/
+let a =true;
+let b = false;
+let c = true;
+console.log ( a&&b&&c);
+console.log ( a&&b||c);
